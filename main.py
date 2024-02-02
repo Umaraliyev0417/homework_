411496fc5d0d4bb12c0ad349f82704ea505f419e
Umaraliyev0417 <umaraliyev0417@gmail.com>
2024-02-02 21:14:03 +0500

    Masalalar

---
+++ main.py
@@ -0,0 +1,131 @@
+# 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst1 = [1, 2, 3, 4, 5]
+# lst2 = [0, 1, 2, 3, 4]
+# i = 0
+# while i < len(lst1):
+#     x = lst1[i]
+#     i += 1
+# # for n in range(len(lst1)):
+#     if lst1[x] == lst2[x+1]:
+#         print(True)
+#         break
+#     else:
+#         print(False)
+#         break
+
+# 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# names = ["Adam", "Sarah", "Malcolm", "Vali"]
+#
+# i = 0
+# res = ''
+# names = sorted(names)
+# while i < len(names):
+#     x = names[i][0]
+#     i += 1
+#     res += x
+# print(res)
+
+# 3 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst = ["red", "blue", "green", "black"]
+# res = []
+# i = 0
+# while i < len(lst):
+#     tst = input("let's go: ")
+#     x = lst[i]
+#     i += 1
+#     if tst == x:
+#         res.append(1)
+#     else:
+#         res.append(-1)
+# print(res)
+
+# 4 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst1 = [3, 2, 5]
+# lst2 = [5, 3, 7, 9, 2]
+# i = 0
+# while i < len(lst1):
+#     x = lst1[i]
+#     i += 1
+#     if x in lst2:
+#         print("True")
+#
+#     else:
+#         print("False")
+
+# 5 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst = ("10201011")
+# lst2 = []
+# i = 0
+# while i < len(lst):
+#     x = lst[i]
+#     i += 1
+#     if x == "1":
+#         lst2.append(True)
+#     elif x == "0":
+#         lst2.append(False)
+# print(lst2)
+
+
+# 6 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst = [1, 5, 3, 9, 7]
+# lst.sort()
+# n = int(input("Son: "))
+# i = 0
+# while i < len(lst):
+#     x = lst[i]
+#     i += 1
+#     if n < len(lst):
+#         print(lst[n - 1])
+#         break
+#     else:
+#         print("None")
+#         break
+
+# 7 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst = []
+# i = 5
+# while i > len(lst):
+#     students_name = input("ism kiriting: ")
+#     lst.append(students_name)
+#     lst.sort()
+#     print(lst)
+
+# 8 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# lst = [3, 3, 3, 2, 1]
+# lst.sort()
+# lst2 = []
+# i = 0
+# while i < len(lst):
+#     x = lst[i]
+#     i += 1
+#     if x not in lst2:
+#         lst2.append(x)
+#     else:
+#         pass
+# print(lst2)
+
+# 9 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# names = ["Frank", "Angela", "Joe"]
+# res = ""
+# i = 0
+# while i < len(names):
+#     x = names[i]
+#     i += 1
+#     res += ("Hello " + x + ", ")
+#
+# print(res[:-2])
+
+# 10 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# total_volume = ([2, 2, 2], [1, 1, 1])
+# sum = 0
+# i = 0
+# while i < len(total_volume):
+#     x = total_volume[i]
+#     i += 1
+#     m = 1
+#     while m < len(x):
+#         n = x[m]
+#         m += 1
+#         m = m * n
+#         sum += m
+# print(sum)
